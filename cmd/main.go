02b0a94d76308f3eef3126fce116918cfafc8a38
Teranikys <97176914+Teranikys@users.noreply.github.com>
2023-03-21 11:55:09 +0300

    fixed error handling

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"regexp"
@@ -17,18 +18,18 @@ func main() {
 		fmt.Println("Введите арифметическое выражение, состоящее из двух операндов и одного оператора через пробел.")
 		fmt.Println("Введите 0, чтобы выйти из программы:")
 		input, _ := reader.ReadString('\n')
-		result, err := StringProccessing(input)
-		if err == 1 {
-			fmt.Println(result)
+		result, err := StringProcessing(input)
+		if err != nil {
+			fmt.Println(err.Error())
 			break
 		}
 		fmt.Printf("Результат: %v\n", result)
 	}
 }
 
-func StringProccessing(input string) (string, int) {
-	var exitFlag, err int
+func StringProcessing(input string) (string, error) {
 	var result string
+	var err error
 	input = strings.TrimSpace(input)
 	splittedInput := strings.Split(input, " ")
 	// Используем регулярное выражение для проверки на соответствие формату математической операции - два операнда
@@ -41,71 +42,64 @@ func StringProccessing(input string) (string, int) {
 		if len(splittedInput) == 1 {
 			exitCode, _ := strconv.Atoi(input)
 			if exitCode == 0 {
-				return result, 1
+				err = errors.New("выход из программы")
+				return result, err
 			}
 		}
-		result = "Вывод ошибки, так как строка не является математической операцией."
-		return result, 1
+		err = errors.New("вывод ошибки, так как строка не является математической операцией")
+		return result, err
 	} else
 	// Проверка на соответствие формату математической операции - два операнда и один оператор (+, -, /, *)
 	if !matched {
-		result = "Вывод ошибки, так как формат математической операции не удовлетворяет заданию — два " +
-			"операнда и один оператор (+, -, /, *)."
-		return result, 1
+		err = errors.New("вывод ошибки, так как формат математической операции не удовлетворяет заданию — два " +
+			"операнда и один оператор (+, -, /, *)")
+		return result, err
 	}
 	// Проверка на римскую систему счисления
 	if strings.ContainsAny(input, "VIX") {
 		// Оба ли операнда являются числами в римской системе счисления
 		if strings.ContainsAny(splittedInput[0], "VIX") != strings.ContainsAny(splittedInput[2], "VIX") {
-			result = "Вывод ошибки, так как используются одновременно разные системы счисления."
-			return result, 1
+			err = errors.New("вывод ошибки, так как используются одновременно разные системы счисления")
+			return result, err
 		}
 		result, err = EvalRoman(splittedInput)
-		if err == 1 {
-			result = "Вывод ошибки, так как числа могут быть только в диапозоне от 1 до 10 включительно, не более"
-			return result, 1
-		} else if err == 2 {
-			result = "Вывод ошибки, так как в римской системе нет отрицательных чисел."
-			return result, 1
-		} else if err == 3 {
-			result = "Вывод ошибки, так как в римской системе нет нуля."
-			return result, 1
+		if err != nil {
+			return result, err
 		}
 	} else {
 		result, err = EvalArab(splittedInput)
-		if err == 1 {
-			result = "Вывод ошибки, так как числа могут быть только в диапозоне от 1 до 10 включительно, не более"
-			return result, 1
+		if err != nil {
+			return result, err
 		}
 	}
 
-	return result, exitFlag
+	return result, nil
 }
 
 // EvalRoman Обработка математических выражений с числами в римской системе счисления
-func EvalRoman(exp []string) (string, int) {
-	var err int
+func EvalRoman(exp []string) (string, error) {
+	var err error
 	exp[0], err = RomanToArab(exp[0])
-	if err == 1 {
+	if err != nil {
 		return "", err
 	}
 	exp[2], err = RomanToArab(exp[2])
-	if err == 1 {
+	if err != nil {
 		return "", err
 	}
 	num, err := EvalArab(exp)
-	if err == 1 {
+	if err != nil {
 		return "", err
 	}
 	res, err := ArabToRoman(num)
-	if err != 0 {
+	if err != nil {
 		return "", err
 	}
-	return res, 0
+	return res, nil
 }
 
 // RomanToArab Перевод чисел из римской СС в арабаскую десятичную
-func RomanToArab(val string) (string, int) {
+func RomanToArab(val string) (string, error) {
 	var res string
 	switch val {
 	case "I":
@@ -129,14 +123,15 @@ func RomanToArab(val string) (string, int) {
 	case "X":
 		res = "10"
 	default:
-		return res, 1
+		err := errors.New("вывод ошибки, так как числа могут быть только в диапозоне от 1 до 10 включительно, не более")
+		return res, err
 	}
 
-	return res, 0
+	return res, nil
 }
 
 // ArabToRoman Перевод чисел из Арабской десятичной СС в Римскую
-func ArabToRoman(val string) (string, int) {
+func ArabToRoman(val string) (string, error) {
 	var res string
 	switch val {
 	case "1":
@@ -162,27 +157,31 @@ func ArabToRoman(val string) (string, int) {
 	default:
 		// Проверка на отрицательность числа
 		if strings.Contains(val, "-") {
-			// Код ошибки для отрицательного ответа в римской СС: 2
-			return res, 2
+			// Ошибка для отрицательного ответа в римской СС
+			err := errors.New("вывод ошибки, так как в римской системе нет отрицательных чисел")
+			return res, err
 		} else
 		// Проверка на равенство нулю
 		if val == "0" {
-			// Код ошибки для нулевого ответа в римской СС: 3
-			return res, 3
+			// Ошибка для нулевого ответа в римской СС
+			err := errors.New("вывод ошибки, так как в римской системе нет нуля")
+			return res, err
 		}
-		// Код ошибки для числа вне диапозона [1; 10]
-		return res, 1
+		// Ошибка для числа вне диапозона [1; 10]
+		err := errors.New("вывод ошибки, так как числа могут быть только в диапозоне от 1 до 10 включительно, не более")
+		return res, err
 	}
 
-	return res, 0
+	return res, nil
 }
 
 // EvalArab Обработка математических выражений в Арабской десятичной системе счисления
-func EvalArab(exp []string) (string, int) {
+func EvalArab(exp []string) (string, error) {
 	x, _ := strconv.Atoi(exp[0])
 	y, _ := strconv.Atoi(exp[2])
 	if x > 10 || y > 10 {
-		return "", 1
+		err := errors.New("вывод ошибки, так как числа могут быть только в диапозоне от 1 до 10 включительно, не более")
+		return "", err
 	}
 	var res string
 	switch exp[1] {
@@ -196,5 +195,5 @@ func EvalArab(exp []string) (string, int) {
 		res = strconv.Itoa(x * y)
 	}
 
-	return res, 0
+	return res, nil
 }
